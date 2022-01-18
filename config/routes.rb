8df9f97d97be37c5c038d699496bb8f55066ffae
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-01-18 12:05:17 +0100

    ✨feat(portfolio): new portfolio with rails and javascript

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# inside config/routes.rb
+Rails.application.routes.draw do
+  get "pages/home"
+  get "pages/profile"
+  get "pages/competences"
+  get "pages/portfolio"
+  get "pages/contact"
+
+  get "projects/front_end"
+  get "projects/full_stack"
+
+  root to: "pages#home"
+end
